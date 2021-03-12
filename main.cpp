a544d01ef851aaa3055871847a7e17430ca9703c
tbourgoin1 <tbourgoin@umass.edu>
2021-03-11 19:57:58 -0500

    developed CLI skeleton for memory implementation

--- main.cpp
+++ main.cpp
@@ -1,1 +1,45 @@
-// rest in peace
+#include <iostream>
+#include <string>
+using namespace std;
+
+int ram[128000000];
+int cache[100];
+
+int write(){
+    cout << "write command" << endl;
+    return 0;
+}
+
+int read(){
+    cout << "read command" << endl;
+    return 0;
+}
+
+int view(){
+    cout << "view command" << endl;
+    return 0;
+}
+
+int main(){
+    while(1){
+        string input;
+        cin >> input;
+        if(input == "w"){
+            write();
+        }
+        else if(input == "r"){
+            read();
+        }
+        else if(input == "v"){
+            view();
+        }
+        else if(input == "exit"){
+            cout << "exiting program..." << endl;
+            break;
+        }
+        else{
+            cout << "please enter a valid command!" << endl;
+        }
+    }
+    return 0;
+}
