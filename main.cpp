415b21d9cdf406bb49631770fe1963e603339fac
tbourgoin1 <tbourgoin@umass.edu>
2021-03-13 12:49:59 -0500

    added clock cycle count and comments for what we need to do still

--- main.cpp
+++ main.cpp
@@ -4,18 +4,29 @@ using namespace std;
 
 int ram[128000000];
 int cache[100];
+int cycles = 0;
 
-int write(){
+// cache at least "16 lines", main memory less than address space
+// need logic for valid/invalid, clean/dirty cache elements
+// need initialization function to set all array elems to 0 - cache lines start invalid and clean
+// cache 0 delay, main memory 3 delay (3 seconds, ms or somewhere between?)
+// keep a count of each W and R command - clock cycle representation (DONE)
+// command line interface (DONE)
+// all memory timing and commands related stuff - slides
+
+int write(){ //respond with "wait" or "done"
+    cycles++;
     cout << "write command" << endl;
     return 0;
 }
 
-int read(){
+int read(){ //respond with "wait" or "done" and return stored value
+    cycles++;
     cout << "read command" << endl;
     return 0;
 }
 
-int view(){
+int view(){ //prints a line of values for address - if level is 1 then also tag, valid, dirty bits
     cout << "view command" << endl;
     return 0;
 }
